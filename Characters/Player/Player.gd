0dcc95bea992e76d7b63d20cd83f119b49138c88
Darr3n <darrensoonyuchien@gmail.com>
2024-03-01 23:22:59 +0800

    finalized animations part 2
    -added water droplet image as temp cursor

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name Player
 
-
+@onready var Body_anim = $Body
 @export var max_speed = 400.0
 @export var acceleration = 10000.0
 @export var friction = 800.0
@@ -10,12 +10,13 @@ var knockback = Vector2.ZERO
 var moving : bool
 
 
-func get_input(left, right, up, down):
-	var input_direction = Input.get_vector(left, right, up, down).normalized()
+func get_input() -> Vector2:
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	return input_direction
 
 func _physics_process(delta):
-	var input = get_input("move_left", "move_right", "move_up", "move_down")
+	var input = get_input()
+	play_animation(input)
 	if input == Vector2.ZERO:
 		if velocity.length() > friction * delta:
 			velocity -= velocity.normalized() * friction * delta
@@ -31,6 +32,25 @@ func _physics_process(delta):
 	Global.player_pos = global_position
 	
 	move_and_slide()
+	
+func play_animation(input : Vector2) -> void:
+	if input.x == -1 or input == Vector2(-1,1 or -1).normalized():
+		Body_anim.play("Body LR")
+		Body_anim.flip_h = true
+		Body_anim.offset.y = 0.5
+	elif input.x == 1 or input == Vector2(1,1 or -1).normalized():
+		Body_anim.play("Body LR")
+		Body_anim.flip_h = false
+		Body_anim.offset.y = 0.5
+	elif input.y == -1:
+		Body_anim.play("Body UD")
+		Body_anim.offset.y = 4
+	elif input.y == 1:
+		Body_anim.play_backwards("Body UD")
+		Body_anim.offset.y = 4
+	else:
+		Body_anim.play("Body")
+		Body_anim.offset.y = 0
 	
 func die() -> void:
 	print("you died")
--- Characters/Player/Water Droplet/WaterDroplet.gd
+++ Characters/Player/Water Droplet/WaterDroplet.gd
@@ -3,11 +3,9 @@ extends Sprite2D
 var speed = 550.0
 var direction : Vector2
 
-
 func _process(delta: float) -> void:
 	if $Timer.is_stopped() == false:
 		global_position += transform.x * speed * delta
-		
 
 func _on_timer_timeout() -> void:
 	queue_free()
